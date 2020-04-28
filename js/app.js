e05b43488eee68e5b02c360a3cefd01c03886c55
SBALDOCK <stephenbaldock78@gmail.com>
2020-04-28 16:22:48 -0700

    Completed 5 yes or no questions in JS

--- js/app.js
+++ js/app.js
@@ -1,47 +1,64 @@
 'use strict';
 
-var name = prompt('Welcome to my site! What is your name?')
-    console.log('User name is ' + name);
-var firstTime = prompt('Well hello there ' + name + '. Is this your first time visiting my page?')
-    if(firstTime.toLowerCase() === 'yes' || firstTime.toLowerCase() === 'y'){
-      alert('I am glad you dropped by today ' + name + '. There is a first time for everything!')
-    } else if(firstTime.toLowerCase() === 'no' || firstTime.toLowerCase() === 'n'){
-      alert('Well that is interesting because I literally created this page a few minutes ago.')
-    } else {
-      alert('I asked a simply yes or no question. Do you mind accomodating me ' + name + '? Thanks! ')
-    } 
+  var name = prompt('Welcome to my site! What is your name?');
+    console.log('Their name is ' + name);
 
-    var favoriteGenre = prompt('Okay, let\'s talk about music. Do you have a favorite type?')
-    if(favoriteGenre.toLowerCase() === 'yes' || favoriteGenre.toLowerCase() === 'y'){
-     var country = prompt('Let me guess. Is it country?')
-        if(country.toLowerCase() === 'yes' || country.toLowerCase() === 'y'){
-          var classicCountry = prompt('Me too! Do you enjoy classic country ' + name + '?')
-            if(classicCountry.toLowerCase() === 'yes' || classicCountry.toLowerCase() === 'y'){
-              alert('Nice. It is my favorite kind of country.')
-            } else { 
-              alert('You might like Buck Owens. What a voice!')
-            }
-        } else {
-          alert('Got it. You should check out some Buck Owens. You might like it.')
-        }
-   } else {
-     var hipHop = prompt('Really? Come on, tell me your favorite genre. Do you like Hip Hop?')
-    } if(hipHop.toLowerCase() === 'yes' || hipHop.toLowerCase() === 'y'){
-     var digablePlanets = prompt('Dig that. I really love early 90\'s hip hop. Are you familiar with Digable Plants?')
-     if(digablePlanets.toLowerCase() === 'yes' || digablePlanets.toLowerCase() === 'yes'){
-       prompt('Ah, I see you have good taste!')
-     } else if(digablePlanets.toLowerCase() === 'no' || digablePlanets.toLowerCase() === 'n'){
-       prompt('I love them. Check out the song Art Of Easing for a good first listen.')
-     } else {
-       prompt('Sorry, I didn\'t catch that. No problem! Give Digable Planets a listen when you have a chance!')
-     }
-  }
-     else {
-     alert('I recommend checking out A Tribe Called Quest. They are one of my favorite groups.')
-   }
+  var myName = prompt('Well hello there ' + name + '. Quiz time! Is my name Hinkle McKrinkle?');
+      if(myName.toLowerCase() === 'yes' || myName.toLowerCase() === 'y'){
+      alert('Close! It is Steve. I can see how you would get the two confused.');
+      } else if(myName.toLowerCase() === 'no' || myName.toLowerCase() === 'n'){
+      alert('That\'s right. It is Steve.');
+      } else {
+      alert('Close enough! Let\'s move on!');
+      }
+      console.log('Is my name Hinkle McKrinkle? ' + name + ' says ' + myName);
+   
+  var humanOrAlpaca = prompt('Okay, next question. Am I an alpaca?')
+      if(humanOrAlpaca.toLowerCase() === 'yes' || humanOrAlpaca.toLowerCase() === 'y'){
+        alert('Very funny ' + name + '.'); 
+      } else if(humanOrAlpaca.toLowerCase() === 'no' || humanOrAlpaca.toLowerCase() === 'n'){
+        alert('Correct. I\'m a close cousin of the alpaca, a human.');
+      } else {
+        alert('I will take a wild guess and assume that you do not think I\'m a cute furry alpaca.');
+      }
+      console.log('Am I an alpaca? ' + name + ' says ' + humanOrAlpaca);
 
-    
+  var likeMusic = prompt('Take a wild guess. Do you think I enjoy music?')
+      if(likeMusic.toLowerCase() === 'yes' || likeMusic.toLowerCase() === 'y'){
+        alert('You would be correct. In that respect, I am like just about everyone else in the world. So unique.');
+      } else if(likeMusic.toLowerCase() === 'no' || likeMusic.toLowerCase() === 'n'){
+        alert('Of course I do. And in fact, I have never met a person nor alpaca who does not enjoy listening to music.')
+      } else {
+        alert('Have you seen the movie Lost In Translation? I think that just happened.')
+      }
+      console.log('Do I enjoy music? ' + name + ' says ' + likeMusic);
 
+  var needHaircut = prompt('Do you think I need a haircut? I was trying to grow a sweet fashion mullet before this pandemic destroyed my stylist\'s career and now my mop is starting to get out of hand.');
+      if(needHaircut.toLowerCase() === 'yes' || needHaircut.toLowerCase() === 'y'){
+        alert('Hey, take it easy. Maybe you should look at yourself in the mirror ' + name + '.');
+      } else if(needHaircut.toLowerCase() === 'no' || needHaircut.toLowerCase() === 'n'){
+        alert('You are probably right. I can always cut it myself if I get too desperate.');
+      } else {
+        alert('Isn\'t hair weird? I mean, it just keeps growing and growing out of you. It never stops!');
+      }
+      console.log('Do I need a haircut? ' + name + ' says ' + needHaircut);
+  
+  var goodDeveloper = prompt('What are your thoughts on my code? Do you think I might make a good developer someday?');
+      if(goodDeveloper.toLowerCase() === 'yes' || goodDeveloper.toLowerCase() === 'y'){
+        alert('Say, thanks. I appreciate the vote of confidence ' + name + '. I will be rooting for you too!');
+      } else if(goodDeveloper.toLowerCase() === 'no' || goodDeveloper.toLowerCase() ==='n'){
+        alert('That\'s just like, you\'re opinion man.');
+      } else {
+        alert('I hereby challenge you to a code-off!');
+      }
+      console.log('Can I make a good developer? ' + name + ' says ' + goodDeveloper);
+
+      alert('Once again, welcome to my page ' + name + '. You are quite a gem.');
+
+      
+
+
+  
 
 
   
